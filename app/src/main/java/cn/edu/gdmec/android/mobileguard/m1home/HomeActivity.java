eda56746b46cebafb1f0cd0031c9531aba1595e1
luoruifeng <1462778722@qq.com>
2017-12-03 17:42:27 +0800

    aa

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -24,6 +24,7 @@ import cn.edu.gdmec.android.mobileguard.m3communicationguard.SecurityPhoneActivi
 import cn.edu.gdmec.android.mobileguard.m4appmanager.AppManagerActivity;
 import cn.edu.gdmec.android.mobileguard.m5virusscan.VirusScanActivity;
 import cn.edu.gdmec.android.mobileguard.m6cleancache.CacheClearListActivity;
+import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.TrafficMonitoringActivity;
 
 public class HomeActivity extends AppCompatActivity {
     private GridView gv_home;
@@ -66,6 +67,10 @@ public class HomeActivity extends AppCompatActivity {
                     case 4:
                         //缓存清理
                         startActivity(CacheClearListActivity.class);
+                        break;
+                    case 6:
+                        //流量统计
+                        startActivity ( TrafficMonitoringActivity.class );
                         break;
 
                 }
