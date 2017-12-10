bf33c32c61f752bcf7f7bb5343f8b4f36276cd4c
luoruifeng <1462778722@qq.com>
2017-12-10 13:59:56 +0800

    hss

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/entity/SmsInfo.java
@@ -0,0 +1,12 @@
+package cn.edu.gdmec.android.mobileguard.m9advancedtools.entity;
+
+/**
+ * Created by LRF on 2017/12/10.
+ */
+
+public class SmsInfo {
+    public  String body;
+    public  String date;
+    public  String type;
+    public String address;
+}
